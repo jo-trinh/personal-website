aa377e8b48664bf9255c587a30aa1fbc12e29026
Jo Trinh <jotrinh1@gmail.com>
2020-04-12 19:20:00 -0400

    Removes nextjs config

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@
 // (But you could use ES2015 features supported by your Node.js version)
 
 const debug = process.env.NODE_ENV !== 'production'
-const withCSS = require('@zeit/next-css')
 
-module.exports = withCSS({
+module.exports = {
   assetPrefix: !debug ? '/personal-website/' : '',
-  cssModules: true,
-})
+}
