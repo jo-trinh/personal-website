82bdddbd7c66073397424259ef5c8d9796bc8faa
Jo Trinh <jotrinh1@gmail.com>
2020-04-12 19:10:06 -0400

    Adds js import

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
 // (But you could use ES2015 features supported by your Node.js version)
 
 const debug = process.env.NODE_ENV !== 'production'
+const withCSS = require('@zeit/next-css')
 
 withCSS({
   assetPrefix: !debug ? '/personal-website/' : '',
