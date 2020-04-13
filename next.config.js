da9675e2c500ea438ceb3d7525a24f2e0eafa274
Jo Trinh <jotrinh1@gmail.com>
2020-04-12 20:06:14 -0400

    Fixes next config typo

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-v // This file is not going through babel transformation.
+// This file is not going through babel transformation.
 // So, we write it in vanilla JS
 // (But you could use ES2015 features supported by your Node.js version)
 
