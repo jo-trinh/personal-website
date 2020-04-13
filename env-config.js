3553c79a91d80999c0e85020cdfe1d5695f3c99e
Jo Trinh <jotrinh1@gmail.com>
2020-04-12 20:20:26 -0400

    Stealing webpack

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? '/jotrinh.dev' : '',
+  'process.env.BACKEND_URL': prod ? 'jotrinh.dev' : '',
 }
