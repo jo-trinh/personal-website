a3e0d5b9d41bd755d103f3897bb9e7ebf38a062b
Jo Trinh <jotrinh1@gmail.com>
2020-04-12 20:03:55 -0400

    Unignores build files

---
+++ out/_next/static/webpack/static/development/pages/_app.js.11c6d4416c01bba832f7.hot-update.js
@@ -0,0 +1,18 @@
+webpackHotUpdate("static/development/pages/_app.js",{
+
+/***/ "./node_modules/next/node_modules/css-loader/dist/cjs.js?!./node_modules/next/dist/compiled/postcss-loader/index.js?!./stylesheets/global.css":
+/*!*****************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/node_modules/css-loader/dist/cjs.js??ref--5-oneOf-5-1!./node_modules/next/dist/compiled/postcss-loader??__nextjs_postcss!./stylesheets/global.css ***!
+  \*****************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../node_modules/next/node_modules/css-loader/dist/runtime/api.js */ "./node_modules/next/node_modules/css-loader/dist/runtime/api.js")(true);
+// Module
+exports.push([module.i, "html {\n  height: 100vh;\n}\nbody {\n  margin: 0px;\n  background-color: red;\n}\n", "",{"version":3,"sources":["/Users/Jo/workspaces/projects/personal-website/stylesheets/global.css"],"names":[],"mappings":"AAAA;EACE,aAAa;AACf;AACA;EACE,WAAW;EACX,qBAAqB;AACvB","file":"global.css","sourcesContent":["html {\n  height: 100vh;\n}\nbody {\n  margin: 0px;\n  background-color: red;\n}\n"]}]);
+
+
+/***/ })
+
+})
+//# sourceMappingURL=_app.js.11c6d4416c01bba832f7.hot-update.js.map
