5cc3110ceaf9b3f0054c2904e14b99a50fd826d8
Jo Trinh <jotrinh1@gmail.com>
2020-04-18 20:34:13 -0400

    publish: Merge pull request #5 from jo-trinh/dependabot/npm_and_yarn/clean-css-4.1.11
    generated from commit 2ea299ae72e4753d81ecc6b986936c5e5abe200b

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b79e182800758d43ced7";
+/******/ 	var hotCurrentHash = "416e5a27e10be0568dc6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
